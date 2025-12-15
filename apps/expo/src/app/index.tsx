08e0d74831094bcf95cf853d1f45989e4b98c3f0
Shawn Rodgers <me@bentsignal.com>
2025-12-15 16:18:30 -0500

    native build working on ios

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -7,7 +7,9 @@ export default function Home() {
   const posts = useQuery(api.posts.getAll);
   return posts?.map((post) => (
     <View key={post._id} className="flex flex-col gap-2">
-      <Text>{post.title}</Text>
+      <Text className="px-4" style={{ color: "red" }}>
+        {post.title}
+      </Text>
       <Text>{post.content}</Text>
     </View>
   ));
