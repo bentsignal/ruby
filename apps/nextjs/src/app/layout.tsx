c49943606f68aae8854c16f58328c793bc5af111
Shawn Rodgers <me@bentsignal.com>
2025-12-15 17:38:31 -0500

    update next metadata

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -13,22 +13,22 @@ import "~/app/styles.css";
 export const metadata: Metadata = {
   metadataBase: new URL(
     env.VERCEL_ENV === "production"
-      ? "https://turbo.t3.gg"
+      ? "https://ruby.travel"
       : "http://localhost:3000",
   ),
-  title: "Create T3 Turbo",
-  description: "Simple monorepo with shared backend for web & mobile apps",
-  openGraph: {
-    title: "Create T3 Turbo",
-    description: "Simple monorepo with shared backend for web & mobile apps",
-    url: "https://create-t3-turbo.vercel.app",
-    siteName: "Create T3 Turbo",
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@jullerino",
-    creator: "@jullerino",
-  },
+  title: "Ruby",
+  description: "A place to share your adventures.",
+  // openGraph: {
+  //   title: "Create T3 Turbo",
+  //   description: "Simple monorepo with shared backend for web & mobile apps",
+  //   url: "https://create-t3-turbo.vercel.app",
+  //   siteName: "Create T3 Turbo",
+  // },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   site: "@jullerino",
+  //   creator: "@jullerino",
+  // },
 };
 
 export const viewport: Viewport = {
