b09a5889635c232a79396a600fe12474d3ca21d6
Shawn Rodgers <me@bentsignal.com>
2025-12-15 15:42:48 -0500

    update expo config

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -2,9 +2,9 @@ import type { ConfigContext, ExpoConfig } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: "expo",
-  slug: "expo",
-  scheme: "expo",
+  name: "Ruby",
+  slug: "ruby",
+  scheme: "ruby",
   version: "0.1.0",
   orientation: "portrait",
   icon: "./assets/icon-light.png",
@@ -15,7 +15,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   newArchEnabled: true,
   assetBundlePatterns: ["**/*"],
   ios: {
-    bundleIdentifier: "your.bundle.identifier",
+    bundleIdentifier: "com.directedbyshawn.rubyapp",
     supportsTablet: true,
     icon: {
       light: "./assets/icon-light.png",
@@ -23,18 +23,18 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     },
   },
   android: {
-    package: "your.bundle.identifier",
+    package: "com.directedbyshawn.rubyapp",
     adaptiveIcon: {
       foregroundImage: "./assets/icon-light.png",
       backgroundColor: "#1F104A",
     },
     edgeToEdgeEnabled: true,
   },
-  // extra: {
-  //   eas: {
-  //     projectId: "your-eas-project-id",
-  //   },
-  // },
+  extra: {
+    eas: {
+      projectId: "b04e9670-d3a9-4186-aeab-dc61c8b07553",
+    },
+  },
   experiments: {
     tsconfigPaths: true,
     typedRoutes: true,
