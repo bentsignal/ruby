af2ac17143dbdd67300afe2419632b30484ec65d
Shawn Rodgers <me@bentsignal.com>
2026-01-02 13:02:17 -0500

    upgrade convex-env to 2.2.0

--- packages/convex/src/convex.env.ts
+++ packages/convex/src/convex.env.ts
@@ -2,8 +2,7 @@ import { createEnv } from "convex-env";
 import { v } from "convex/values";
 
 export const env = createEnv({
-  CONVEX_SITE_URL: v.string(),
-  ENVIRONMENT: v.string(),
+  ENVIRONMENT: v.union(v.literal("development"), v.literal("production")),
   SITE_URL: v.string(),
   BETTER_AUTH_SECRET: v.string(),
   GOOGLE_CLIENT_ID: v.string(),
